c91b8d3d863ae5d80d6248f0a392f638b7b7da8e
riefive <muhammad.rifai@salt.co.id>
2024-04-26 13:33:08 +0700

    fixing build error

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,13 +1,22 @@
 'use client'
 
+import { Suspense } from 'react'
 import { useSearchParams } from 'next/navigation'
 import Login from '../../components/Login'
 
-export default function SignIn() {
+function SignInContent() {
     const searchParams = useSearchParams()
     const callbackUrl = searchParams.get('callbackUrl')
 
     return (
         <Login callbackUrl={callbackUrl} />
+    )
+}
+
+export default function SignIn() {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+           <SignInContent />
+        </Suspense>
     )
 };
